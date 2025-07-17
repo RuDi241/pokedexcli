5bdefbcb5a30bc5f6e3dad50782d10eff2497cff
Karim Marayta <maraytakarim@gmail.com>
2025-07-18 02:53:02 +0300

    Fixed panic when unknown command is entered

--- main.go
+++ main.go
@@ -12,6 +12,47 @@ func cleanInput(text string) []string {
 	return strings.Fields(text)
 }
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+// Supported CLI commands
+var commands map[string]cliCommand
+
+func init() {
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	message := "Welcome to the Pokedex!\nUsage:\n\n"
+
+	for _, c := range commands {
+		message += fmt.Sprintf("%v: %v\n", c.name, c.description)
+	}
+
+	fmt.Print(message)
+	return nil
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -23,8 +64,18 @@ func main() {
 		}
 		text := scanner.Text()
 		words := cleanInput(text)
-		if len(words) > 0 {
-			fmt.Printf("Your command was: %v\n", words[0])
+		if len(words) <= 0 {
+			fmt.Println("Enter a command")
 		}
+		if val, ok := commands[words[0]]; ok {
+
+			err := val.callback()
+			if err != nil {
+				fmt.Println("Error encountered")
+			}
+		} else {
+			fmt.Println("Command not found")
+		}
+
 	}
 }
