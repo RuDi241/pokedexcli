0df51cea2e3295366db557a147b24d4d1fb1ee5c
Karim Marayta <maraytakarim@gmail.com>
2025-07-18 02:03:54 +0300

    Created cleanInput function and tests

---
+++ repl_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "   AAA BBB         CCCC",
+			expected: []string{"AAA", "BBB", "CCCC"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+
+		// Check len
+		if len(actual) != len(c.expected) {
+			t.Errorf("Failed test case:\n input: %v\noutput: %v\nexpected: %v\n", c.input, actual, c.expected)
+			t.Fail()
+		}
+
+		// Check every word
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("Failed test case:\n input: %v\noutput: %v\nexpected: %v\n", c.input, actual, c.expected)
+				t.Fail()
+			}
+		}
+	}
+}
