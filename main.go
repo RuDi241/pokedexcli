258379a9f06509d903b7064283bcb865dec9bd3d
Karim Marayta <maraytakarim@gmail.com>
2025-07-18 03:59:40 +0300

    Added map and mapb commands

--- main.go
+++ main.go
@@ -2,24 +2,48 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 )
 
+// Helper functions
 func cleanInput(text string) []string {
 	text = strings.ToLower(text)
 	return strings.Fields(text)
 }
 
+// Structs
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(conf *config) error
+}
+
+type config struct {
+	prev string
+	next string
+}
+
+type locationAreaResponse struct {
+	Count   int            `json:"count"`
+	Next    string         `json:"next"`
+	Prev    string         `json:"previous"`
+	Results []locationArea `json:"results"`
+}
+
+type locationArea struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
 }
 
 // Supported CLI commands
 var commands map[string]cliCommand
+
+var conf config
 
 func init() {
 	commands = map[string]cliCommand{
@@ -33,16 +57,29 @@ func init() {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Displays the next 20 location areas",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays the previous 20 location areas",
+			callback:    commandMapb,
+		},
 	}
+
+	conf.prev = ""
+	conf.next = "https://pokeapi.co/api/v2/location-area"
 }
 
-func commandExit() error {
+func commandExit(conf *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(conf *config) error {
 	message := "Welcome to the Pokedex!\nUsage:\n\n"
 
 	for _, c := range commands {
@@ -50,6 +87,60 @@ func commandHelp() error {
 	}
 
 	fmt.Print(message)
+	return nil
+}
+
+func commandMap(conf *config) error {
+	res, err := http.Get(conf.next)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	var responseData locationAreaResponse
+	json.Unmarshal(body, &responseData)
+
+	conf.prev = conf.next
+	conf.next = responseData.Next
+
+	for _, la := range responseData.Results {
+		println(la.Name)
+	}
+
+	return nil
+}
+
+func commandMapb(conf *config) error {
+	if conf.prev == "" {
+		println("you're on the first page")
+		return nil
+	}
+	res, err := http.Get(conf.prev)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	var responseData locationAreaResponse
+	json.Unmarshal(body, &responseData)
+
+	conf.next = conf.prev
+	conf.prev = responseData.Prev
+
+	for _, la := range responseData.Results {
+		println(la.Name)
+	}
+
 	return nil
 }
 
@@ -69,9 +160,9 @@ func main() {
 		}
 		if val, ok := commands[words[0]]; ok {
 
-			err := val.callback()
+			err := val.callback(&conf)
 			if err != nil {
-				fmt.Println("Error encountered")
+				fmt.Printf("Error encountered: %v\n", err)
 			}
 		} else {
 			fmt.Println("Command not found")
